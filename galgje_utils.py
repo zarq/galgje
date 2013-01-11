b6f787033917932ddc554598e898f685fa35d26a
Ivo Timmermans <ivo@wirescape.org>
2013-01-11 11:57:40 +0100

    stnh

--- galgje_utils.py
+++ galgje_utils.py
@@ -1,6 +1,6 @@
 def debug(s, end='\n'):
     if type(s) != str and type(s) != bytes:
         s = repr(s)
-    if type(s) == str:
-        s = s.encode('utf-8')
+    if type(s) == bytes:
+        s = s.decode('utf-8')
     print(s, end=end)
